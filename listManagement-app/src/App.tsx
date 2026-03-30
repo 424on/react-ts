52ec2479f28af55d9fc5895e7517989613ee60b6
424on <88828652+424on@users.noreply.github.com>
2026-03-30 23:44:41 +0900

    feat: add RecipeList

--- listManagement-app/src/App.tsx
+++ listManagement-app/src/App.tsx
@@ -1,22 +1,34 @@
-import { useState } from 'react';
-import TrafficLight from './components/TrafficLight';
+import {useState} from "react";
 
 const App = () => {
-   const[light, setLight] = useState<string>("red")
-    const onYellow = () => setLight("yellow")
-    const onGreen = () => setLight("green")
-    const onRed = () => setLight("red")
+    const [recipes, setRecipes] = useState([
+        {
+            id: "greek-salad",
+            name: "Greek Salad",
+            ingredients: ["tomatoes", "cucumber", "onion", "olives", "feta"],
+        },
+        {
+            id: "hawaiian-pizza",
+            name: "Hawaiian Pizza",
+            ingredients: [
+                "pizza crust",
+                "pizza sauce",
+                "mozzarella",
+                "ham",
+                "pineapple",
+            ],
+        },
+        {
+            id: "hummus",
+            name: "Hummus",
+            ingredients: ["chickpeas", "olive oil", "garlic cloves", "lemon", "tahini"],
+        },
+    ]);
     return (
-       <>
-           <TrafficLight
-               light={light}
-               onYellow={onYellow}
-               onGreen={onGreen}
-               onRed ={onRed}
-           />
-
-       </>
-   )
+        <>
+            <p>{recipes.map(())}</p>
+        </>
+    )
 };
 
 export default App;
---
+++ listManagement-app/src/components/RecipeList.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const RecipeList = () => {
+    return <></>
+    );
+};
+
+export default RecipeList;
